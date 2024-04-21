c5d5ad178665556ccfe9065e13c575f2a10dcd36
Aashna-Verma <aashna.verma@outlook.com>
2024-04-21 17:43:47 -0400

    box

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import SceneInit from "./SceneInit";
 
 function App() {
-  const [count, setCount] = useState(0)
+	useEffect(() => {
+		const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    test.scene.add(boxMesh);
+
+	}, []);
+
+	return (
+		<div>
+			<canvas id="myThreeJsCanvas"></canvas>
+		</div>
+	);
 }
 
-export default App
+export default App;
